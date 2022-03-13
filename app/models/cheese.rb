654647504505d5f6f35d115e99294a25786b95b7
Olha Gruzglina <olga.zhmura@gmail.com>
2022-03-13 16:28:35 -0400

    commit tree 3139c322e359fcf02d0318f0f60bd0dad4220321

---
+++ app/models/cheese.rb
@@ -0,0 +1,10 @@
+class Cheese < ApplicationRecord
+    def summary
+        "#{self.name}: $#{self.price}"
+    end
+
+    def self.best_sellers
+        cheeses = Cheese.all.select {|cheese| cheese.is_best_seller == true}
+        cheeses.map {|cheese| cheese.name}
+    end
+end
